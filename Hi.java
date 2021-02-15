45845454c33afd4813c1227dd7747bf30cc29dd7
Darwin-3000 <darwinquinde10@gmail.com>
2021-02-14 20:38:14 -0500

    Add existing file

---
+++ Hi.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Hi extends JFrame{
+    public static void main (String...args)
+    {
+
+    
+
+  Hi swingDemo  = new Hi();
+    Hi.setTitle("SwingDemo");
+    Hi.setSize(new Dimension(500,500));
+    Hi.setVisible(true);
+  
+
+
+
+
+    }
+   }
+    
+
+
+
+
+
